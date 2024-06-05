73fdbe0e2f008f880d1795ef2eaa542f05c4cbf2
Augusto Vicente <devguto@gmail.com>
2024-06-05 23:28:36 +0100

    FEAT: stable version

---
+++ README.md
@@ -0,0 +1,53 @@
+# oEmbed Validator
+
+This is a simple utility called "oEmbed Validator". It checks if a given JSON or XML is a valid oEmbed response.
+
+## Installation
+
+```bash
+npm install
+# or
+yarn install
+```
+
+## Usage
+
+To use this utility, simply import it in your JavaScript file and call the main function with the JSON or XML data you want to validate.
+
+```javascript
+// Example usage
+const response = `{
+	"version": "1.0",
+	"type": "video",
+	"provider_name": "YouTube",
+	"provider_url": "https://youtube.com/",
+	"width": 425,
+	"height": 344,
+	"title": "Amazing Nintendo Facts",
+	"author_name": "ZackScott",
+	"author_url": "https://www.youtube.com/user/ZackScott",
+	"html": "<object width=\\"425\\" height=\\"344\\"><param name=\\"movie\\" value=\\"https://www.youtube.com/v/M3r2XDceM6A&fs=1\\"></param><param name=\\"allowFullScreen\\" value=\\"true\\"></param><param name=\\"allowscriptaccess\\" value=\\"always\\"></param><embed src=\\"https://www.youtube.com/v/M3r2XDceM6A&fs=1\\" type=\\"application/x-shockwave-flash\\" width=\\"425\\" height=\\"344\\" allowscriptaccess=\\"always\\" allowfullscreen=\\"true\\"></embed></object>"
+}`;
+
+// Import the function
+const {validateOEmbedResponse} = require('./index');
+
+// Call the function
+validateOEmbedResponse(200, response).then((result) => {
+    console.log('Result:', result);
+}).catch((error) => {
+    console.error('Error:', error);
+});
+```
+
+## Dependencies
+
+This project depends on the `xml2js` library for parsing XML data.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+This project is licensed under the ISC license.
--- index.js
+++ index.js
@@ -51,6 +51,7 @@ const validateOEmbedResponseReturn = {
     INVALID_OEMBED_XML: 'Invalid oEmbed XML response',
     VALID_OEMBED_ERROR: 'Valid oEmbed error response',
     INVALID_OEMBED_ERROR: 'Invalid oEmbed error response',
+    EXTRA_FIELDS: 'Extra fields in oEmbed response',
 };
 
 /**
@@ -66,39 +67,37 @@ async function validateOEmbedResponse(statusCode, response) {
             // Check if the response is a valid JSON
             try {
                 const json = JSON.parse(response);
-                if (json.type === 'oembed') {
-                    const fields = Object.keys(json);
-                    const missingFields = requiredFields.filter(field => !fields.includes(field));
-                    if (missingFields.length === 0) {
-                        const extraFields = fields.filter(field => !requiredFields.includes(field) && !optionalFields.includes(field));
-                        if (extraFields.length === 0) {
-                            resolve(validateOEmbedResponseReturn.VALID_OEMBED_JSON);
-                        } else {
-                            for (const field of extraFields) {
-                                if (!requiredByType[json.type].includes(field)) {
-                                    reject(validateOEmbedResponseReturn.INVALID_OEMBED_JSON);
-                                    return;
-                                }
-                            }
+                const fields = Object.keys(json);
+                const missingFields = requiredFields.filter(field => !fields.includes(field));
+                if (missingFields.length === 0) {
+                    const extraFields = fields.filter(field => !requiredFields.includes(field) && !optionalFields.includes(field));
+                    if (extraFields.length === 0) {
+                        resolve(validateOEmbedResponseReturn.VALID_OEMBED_JSON);
+                    } else {
+                        const not_valid_fields = [];
+                        for (const field of extraFields) {
+                            if (!requiredByType[json.type].includes(field)) {
+                                not_valid_fields.push(field);
 
+                            }
+                        }
+                        if (not_valid_fields.length > 0) {
+                            reject(validateOEmbedResponseReturn.EXTRA_FIELDS + ': ' + not_valid_fields.join(', '));
+                        } else {
                             resolve(validateOEmbedResponseReturn.VALID_OEMBED_JSON);
                         }
-                    } else {
-                        reject(validateOEmbedResponseReturn.INVALID_OEMBED_JSON);
                     }
+                } else {
+                    reject(validateOEmbedResponseReturn.INVALID_OEMBED_JSON);
                 }
             } catch (error) {
-                reject(validateOEmbedResponseReturn.INVALID_OEMBED_JSON);
-            }
-
-            // Check if the response is a valid XML
-            try {
-                parseString(response, (error, result) => {
-                    if (error) {
-                        reject(validateOEmbedResponseReturn.INVALID_OEMBED_XML);
-                    }
-
-                    if (result.oembed.type[0] === 'oembed') {
+                // Check if the response is a valid XML
+                try {
+                    parseString(response, (error, result) => {
+                        if (error) {
+                            reject(validateOEmbedResponseReturn.INVALID_OEMBED_XML);
+                        }
+    
                         const fields = Object.keys(result.oembed);
                         const missingFields = requiredFields.filter(field => !fields.includes(field));
                         if (missingFields.length === 0) {
@@ -106,25 +105,27 @@ async function validateOEmbedResponse(statusCode, response) {
                             if (extraFields.length === 0) {
                                 resolve(validateOEmbedResponseReturn.VALID_OEMBED_XML);
                             } else {
+                                const not_valid_fields = [];
                                 for (const field of extraFields) {
                                     if (!requiredByType[result.oembed.type[0]].includes(field)) {
-                                        reject(validateOEmbedResponseReturn.INVALID_OEMBED_XML);
-                                        return;
+                                        not_valid_fields.push(field);
                                     }
                                 }
-
-                                resolve(validateOEmbedResponseReturn.VALID_OEMBED_XML);
+                                if (not_valid_fields.length > 0) {
+                                    reject(validateOEmbedResponseReturn.EXTRA_FIELDS + ': ' + not_valid_fields.join(', '));
+                                } else {
+                                    resolve(validateOEmbedResponseReturn.VALID_OEMBED_XML);
+                                }
                             }
                         } else {
                             reject(validateOEmbedResponseReturn.INVALID_OEMBED_XML);
                         }
-                    } else {
-                        reject(validateOEmbedResponseReturn.INVALID_OEMBED_XML);
-                    }
-                });
-            } catch (error) {
-                reject(validateOEmbedResponseReturn.INVALID_OEMBED_XML);
-            }   
+                    });
+                } catch (error) {
+                    reject(validateOEmbedResponseReturn.INVALID_OEMBED_XML);
+                }   
+            }
+
         } else if (allowedErrors.includes(statusCode)) {
             resolve(validateOEmbedResponseReturn.VALID_OEMBED_ERROR);
         } else {
